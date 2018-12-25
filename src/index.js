1c902454bce8447cd5dd2200628675b36041583d
Kent Tokunaga <KentTokunaga@gmail.com>
2018-12-24 22:47:37 -0500

    Add authorization to token

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const { GraphQLServer } = require('graphql-yoga');
 const mongoose = require('mongoose');
-const { DATABASE_URL } = require('./config');
+const jwt = require('jsonwebtoken');
+const { DATABASE_URL, JWT_SECRET } = require('./config');
 const resolvers = require('./resolvers');
 
 const typeDefs = './src/schema.graphql';
 
+
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
+  context: incomingData => ({
+    incomingData,
+    isAuthorized: () => {
+      const authHeader = incomingData.request.header('authorization');
+      if (!authHeader) {
+        throw new Error('Unauthorized');
+      }
+      const token = authHeader.replace('Bearer ', '');
+      const decodedToken = jwt.verify(token, JWT_SECRET);
+      return decodedToken;
+    },
+  }),
 });
 
 if (require.main === module) {
